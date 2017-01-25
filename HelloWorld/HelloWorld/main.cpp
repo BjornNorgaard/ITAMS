1fa35553d7fee03ce8641946fa57ea869fa9a142
Bjorn Nørgaard <bjornsnorgaard@gmail.com>
2017-01-25 17:43:35 +0100

    created helloworld project to see if stk500 still works. it does

---
+++ HelloWorld/HelloWorld/main.cpp
@@ -0,0 +1,19 @@
+#define F_CPU 3686400UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+#define PAUSE _delay_ms(500)
+
+int main(void)
+{
+	DDRC = 0xFF;
+	PORTC = 0xFF;
+
+	while (1)
+	{
+		PORTC = 0x00;
+		PAUSE;
+		PORTC = 0xFF;
+		PAUSE;
+	}
+}
