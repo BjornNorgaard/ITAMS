dcdb362a2b3dbdbce5685052268dd37c2b76ca82
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-01-30 13:30:44 +0100

    created solution for lab3

---
+++ labs/lab3/alphanumeric led driver/alphanumeric led driver/main.c
@@ -0,0 +1,18 @@
+/*
+ * alphanumeric led driver.c
+ *
+ * Created: 30-01-2017 13:23:21
+ * Author : Bjorn
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
