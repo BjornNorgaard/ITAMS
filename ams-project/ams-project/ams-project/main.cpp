fcc7439aa952f885847c9a8c0f39efc2620b852f
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-02-23 10:51:39 +0100

    reset main for better structure

--- ams-project/ams-project/ams-project/main.cpp
+++ ams-project/ams-project/ams-project/main.cpp
@@ -2,34 +2,13 @@
 #include <avr/io.h>
 #include <util/delay.h>
 #include "led.h"
-#include "switch"
+#include "switch.h"
 #include "uart.h"
-
-#define CTRL_Z 26
-
-void SendMessage(char* message, char* phonenumber);
 
 int main(void)
 {
-	InitUART(9600, 8);
-	initLEDport(2);
-
-	char message[50] = "Hello world!";
-	char phonenumber[11] = "+4513371337";
-
 	/* Replace with your application code */
 	while (1)
 	{
-		SendMessage(message, phonenumber);
-		_delay_ms(5000);
 	}
-}
-
-
-void SendMessage(char* message, char* phonenumber)
-{
-	SendString("AT+CMGS=");
-	SendString(phonenumber);
-	SendString(message);
-	SendChar(CTRL_Z);
 }
