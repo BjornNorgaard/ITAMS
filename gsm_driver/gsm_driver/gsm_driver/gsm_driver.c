b89560739fe1e61f67ded8cbfc8f76408878f7b3
Bjørn Nørgaard <bjornsnorgaard@gmail.com>
2017-03-09 22:51:39 +0100

    added functions for setting echo, textmode, smsconfig and waiting for response

--- gsm_driver/gsm_driver/gsm_driver/gsm_driver.c
+++ gsm_driver/gsm_driver/gsm_driver/gsm_driver.c
@@ -1,6 +1,41 @@
 #include "gsm_driver.h"
+#include "uart.h"
+
+#define F_CPU 3868400
+#include <util/delay.h>
 
 void gsmInit()
 {
+	gsmSetEcho(1);
+	gsmSetTextMode();
+	gsmConfigSms();
+}
 
+void gsmWaitForResponse()
+{
+	_delay_ms(210);
+}
+
+void gsmSetEcho(unsigned char echo)
+{
+	uartSendString("ATE");
+	uartSendString(echo);
+	uartSendString("\r");
+
+	gsmWaitForResponse();
+}
+
+void gsmSetTextMode()
+{
+	uartSendString("AT+CMGF=1");
+	uartSendString("\r");
+
+	gsmWaitForResponse();
+}
+
+void gsmConfigSms()
+{
+	uartSendString("AT+CNMI=3,1\r");
+
+	gsmWaitForResponse();
 }
--- gsm_driver/gsm_driver/gsm_driver/gsm_driver.h
+++ gsm_driver/gsm_driver/gsm_driver/gsm_driver.h
@@ -1,6 +1,13 @@
 #ifndef GSM_DRIVER_H_
 #define GSM_DRIVER_H_
 
+// main functions
 void gsmInit();
+
+// util functions
+void gsmWaitForResponse();
+void gsmSetEcho(unsigned char echo);
+void gsmSetTextMode();
+void gsmConfigSms();
 
 #endif /* GSM_DRIVER_H_ */
